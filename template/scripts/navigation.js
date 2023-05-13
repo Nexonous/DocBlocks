e19d59990ccb4515bf1e7ac1cdcb0051997a7e20
Dhiraj Wishal <wishaldhiraj@gmail.com>
2023-05-13 19:24:37 +0530

    Improved styling for the desktop and mobile apps.

--- template/scripts/navigation.js
+++ template/scripts/navigation.js
@@ -1,9 +1,3 @@
 for (const element of document.getElementById('navigation').getElementsByTagName('a')) {
   if (element.href === document.URL) { element.classList.add('active') }
 }
-
-window.onload = () => {
-  setTimeout(() => {
-    document.getElementById('navigation-bar').classList.remove('expand')
-  }, 1000)
-}
