43e99199357ded3be6ed6dffc2d8e55e7d4aa562
Dhiraj Wishal <wishaldhiraj@gmail.com>
2023-05-14 18:42:39 +0530

    Added the Peregrine controller as a submodule.
    * Added the Peregrine controller as a submodule for documentation.
    * Updated the generator's source to accommodate the controller and it's latest version.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import Input from './input'
 import Version from './version'
 
 const generator = new Generator('public', 'content/Index.md', 'content/Not Found.md', [
-  new Input('Documentation Tool').addVersion(Version.fromDirectory('docs'))
+  new Input('Documentation Tool').addVersion(Version.fromDirectory('docs')),
+  new Input('Peregrine').addVersion(Version.fromDirectory('content/Peregrine/latest'))
 ])
 generator.generate()
